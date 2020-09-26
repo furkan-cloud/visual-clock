d1f263d23550df1579ac617fe40265147a59a146
furkan-cloud <firstinfirstout2020@gmail.com>
2020-09-26 15:00:00 +0300

    feat js file setinterval for time

--- myScript.js
+++ myScript.js
@@ -1,34 +1,35 @@
 function startTime() {
-    let today = new Date();
+  let today = new Date();
 
-    let hour = today.getHours();
+  let hour = today.getHours();
 
-    let min = today.getMinutes();
+  let min = today.getMinutes();
 
-    let sec = today.getSeconds();
+  let sec = today.getSeconds();
 
-    let amPm= "AM"
-   
-    if (hour >= 12) {
-        hour -= 12;
-        amPm = "PM";
-      }if (hour === 0) {
-        hour = 12;
-      }
-    hour = checkTime(hour);
-    min = checkTime(min);
-    sec = checkTime(sec);
+  let amPm = "AM";
 
-    document.getElementById("digitalClock").innerHTML =
-      hour + ":" + min + ":" + sec + " " + amPm;
-
-    document.getElementById("date").innerHTML = today.toLocaleDateString();
-
-    setInterval(startTime, 2000);
+  if (hour >= 12) {
+    hour -= 12;
+    amPm = "PM";
   }
-  function checkTime(i) {
-    if (i < 10) {
-      i = "0" + i;
-    } // add zero in front of numbers < 10
-    return i;
+  if (hour === 0) {
+    hour = 12;
   }
+  hour = checkTime(hour);
+  min = checkTime(min);
+  sec = checkTime(sec);
+
+  document.getElementById("digitalClock").innerHTML =
+    hour + ":" + min + ":" + sec + " " + amPm;
+
+  document.getElementById("date").innerHTML = today.toLocaleDateString();
+
+  setInterval(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  } // add zero in front of numbers < 10
+  return i;
+}
